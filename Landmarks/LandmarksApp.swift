ff93c6a04e46fa49e9e7ac878aee0d5dac5c231b
parkhohyun <haor3@hanmail.net>
2022-11-14 12:53:15 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 박호현 on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
